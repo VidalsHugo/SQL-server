3957eaa456cff31e83da7e48ff80236228a55f65
Hugo Vidal <vidalshugo1@gmail.com>
2023-03-31 19:52:54 -0300

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,31 @@
+create table Ecomerce(
+
+	Codigo int not null,
+	Nome varchar(100) not null,
+	TipoPessoa char not null
+)
+
+use Ecomerce;
+
+select *
+from Cliente;
+
+insert Cliente values(1, 'Hugo', 'F'),
+(2, 'Max', 'F'), 
+(3, 'Josue', 'J'),
+(4, 'Luiz', 'F'),
+(5, 'Juj', 'F');
+
+update Cliente
+set TipoPessoa = 'F'
+where TipoPessoa = 'J';
+
+delete from Cliente
+where Codigo in(5,4,3,2);
+
+insert Cliente values(1, 'Vidal', 'J');
+
+select *
+from Cliente
+where Codigo = 1
+AND TipoPessoa = 'J';
