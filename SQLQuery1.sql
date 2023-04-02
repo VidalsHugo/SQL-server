e637e8acb33207c100fddc82efbea0359a8d798e
Hugo Vidal <87623017+VidalsHugo@users.noreply.github.com>
2023-04-02 18:27:16 -0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,7 +6,7 @@ create table Cliente(
 	Codigo int not null,
 	Nome varchar(100) not null,
 	TipoPessoa char not null
-)
+);
 
 select *
 from Cliente;
@@ -30,3 +30,48 @@ select *
 from Cliente
 where Codigo = 1
 AND TipoPessoa = 'J';
+
+
+create table Produtos(
+
+	Codigo int not null,
+	Nome varchar(100) not null,
+	Descricao varchar(200) not null,
+	Preco float not null
+);
+
+select *
+from Produtos;
+
+insert Produtos values
+(1, 'Caneta', 'Azul', 2.50),
+(2, 'Caderno', 'Caderno 10 materias', 20.99);
+
+create table Pedido(
+
+	Codigo int not null,
+	DataSolicitacao datetime not null,
+	FlagPago bit not null,
+	TotalPedido float not null,
+	CodigoCliente int not null
+);
+
+select *
+from Pedido;
+
+insert Pedido values(1, GETDATE(), 0, 3, 7);
+
+create table PedidoItem(
+
+	Codigo int not null,
+	CodigoProduto int not null,
+	Preco float not null,
+	Quantidade int not null,
+
+);
+
+
+select *
+from PedidoItem;
+
+insert PedidoItem values(1, 1, 2.50, 2);
